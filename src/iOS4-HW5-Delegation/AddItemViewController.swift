8a1722dbbb02073759d2660dc2cf577dda939965
Apoorva <apoorvamoodbidri14@gmail.com>
2016-01-25 19:57:53 -0800

    updates to hw5

--- src/iOS4-HW5-Delegation/AddItemViewController.swift
+++ src/iOS4-HW5-Delegation/AddItemViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+// declare protocol
+protocol SaveNewData {
+    func saveData (data:String)
+}
 
 class AddItemViewController: UIViewController {
 
     @IBOutlet weak var itemTextField: UITextField!
+    
+    // declare variable for delegated work 
+    var delegate:SaveNewData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func saveButtonPressed(sender: AnyObject) {
+        // call the method on delegate 
+        
+        delegate?.saveData(itemTextField.text!)
+        
+        // pop the top viewController 
+        navigationController?.popViewControllerAnimated(true)
+        
+        
     }
 }
--- src/iOS4-HW5-Delegation/ItemsTableViewController.swift
+++ src/iOS4-HW5-Delegation/ItemsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ItemsTableViewController: UITableViewController {
+class ItemsTableViewController: UITableViewController,SaveNewData {   //adopt the protocol
 
     var items:[String] = []
     
@@ -16,36 +16,46 @@ class ItemsTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
+    //conforming the protocol
+    func saveData(data: String) {
+        items.append(data)
+        tableView.reloadData() // to reload the table
+    }
+    
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+  /*  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
-    }
+    } */
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("itemCellIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("todoCellIdentifier", forIndexPath: indexPath)
     
         cell.textLabel?.text = items[indexPath.row]
         
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        let destinationViewController = segue.destinationViewController as! AddItemViewController
+        
+        destinationViewController.delegate = self
+       
     }
-    */
+    
 
 }
